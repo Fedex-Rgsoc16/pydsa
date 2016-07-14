0d8330d7185b57b3db5e57b9b5b4ffe8ee529adc
sahar2012 <sahar2012@gmail.com>
2016-07-15 00:56:37 +0530

    Corrected PEP8 errors for Longest Increasing Subsequence

--- pydsa/tests/test_longest_inc_subsequence.py
+++ pydsa/tests/test_longest_inc_subsequence.py
@@ -1,6 +1,7 @@
 from pydsa.longest_inc_subsequence import longest_inc_subsequence
 
+
 def test_longest_inc_subsequence():
-	arr = [1, 12, 7, 0, 23, 11, 52, 31, 61, 69, 70, 2]
-	result = longest_inc_subsequence(arr)
-	assert result == 7
+    arr = [1, 12, 7, 0, 23, 11, 52, 31, 61, 69, 70, 2]
+    result = longest_inc_subsequence(arr)
+    assert result == 7
