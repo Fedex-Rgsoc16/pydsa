783f727fff4d1e874b84870ed44d8eb52ac26721
Mansi Goel <mansi14062@iiitd.ac.in>
2016-07-15 00:56:04 +0530

    fixed pep8 errors in test file

--- pydsa/tests/test_counting_inversions.py
+++ pydsa/tests/test_counting_inversions.py
@@ -1,6 +1,7 @@
 from pydsa.counting_inversions import counting_inversions
 
+
 def test_counting_inversions():
-	a = [1, 20, 6, 4, 5]
-	b = counting_inversions(a)
-	assert b == 5
+    a = [1, 20, 6, 4, 5]
+    b = counting_inversions(a)
+    assert b == 5
